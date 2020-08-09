628fa013b6f3dde53dfb10385d2364fedb430a54
Wouter Wijsman <wwijsman@live.nl>
2020-08-09 17:35:00 +0200

    Added missing semicolons

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -5,9 +5,9 @@ GameState::GameState() : board(Board(0, 0, 8, 7)) {}
 GameState::~GameState() {}
 
 void GameState::update() {
-    this->board.update()
+    this->board.update();
 }
 
 void GameState::draw(SDL_Renderer *renderer) {
-    this->board.draw(renderer)
+    this->board.draw(renderer);
 }
