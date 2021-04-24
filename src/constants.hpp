7b8d8abf46d908d3a9fae99bcfff423f290b8bb2
Wouter Wijsman <wwijsman@live.nl>
2021-04-24 17:34:38 +0200

    Change default size

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,9 +1,9 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-inline constexpr int SCREEN_WIDTH = 800;
-inline constexpr int SCREEN_HEIGHT = 600;
+inline constexpr int SCREEN_WIDTH = 769;
+inline constexpr int SCREEN_HEIGHT = 673;
 
-const int GEM_SIZE = 64;
+const int GEM_SIZE = 96;
 
 #endif // CONSTANTS_HPP
