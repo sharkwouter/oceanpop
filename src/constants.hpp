db1dc266ca7ea9f02c2ac54597cdc9abe4280238
Wouter Wijsman <wouter.wijsman@qbeyond.de>
2021-11-24 13:37:51 +0100

    Initial port

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,11 +1,16 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
+inline constexpr int DROP_TIMER = 250;
+
+#ifndef __vita__
 inline constexpr int SCREEN_WIDTH = 769;
 inline constexpr int SCREEN_HEIGHT = 673;
-
 inline constexpr int GEM_SIZE = 96;
-
-inline constexpr int DROP_TIMER = 250;
+#else
+inline constexpr int SCREEN_WIDTH = 960;
+inline constexpr int SCREEN_HEIGHT = 544;
+inline constexpr int GEM_SIZE = 64;
+#endif
 
 #endif // CONSTANTS_HPP
