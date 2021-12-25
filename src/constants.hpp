e627d40cd1a6aa5f1f4c69a9f7ca2541a0b3bb05
Wouter Wijsman <wouter.wijsman@qbeyond.de>
2021-12-25 13:32:49 +0100

    Make drop and match times feel better

--- src/constants.hpp
+++ src/constants.hpp
@@ -23,11 +23,11 @@ inline constexpr int SHELL_SIZE = 128;
 inline constexpr int FONT_SIZE = SHELL_SIZE/2;
 inline constexpr int FONT_SIZE_TITLE = SHELL_SIZE;
 
-inline constexpr int DROP_TIME = 400;
+inline constexpr int DROP_TIME = 300;
 inline constexpr int DROP_STEPS = SHELL_SIZE;
 inline constexpr float DROP_DELAY = DROP_TIME/DROP_STEPS;
 
-inline constexpr int MATCH_TIME = 1000;
+inline constexpr int MATCH_TIME = 500;
 inline constexpr int MATCH_STEPS = 60;
 inline constexpr float MATCH_DELAY = MATCH_TIME/MATCH_STEPS;
 
