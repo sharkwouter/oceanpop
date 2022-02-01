3c670421692435725126e0273edd23996720c752
Wouter Wijsman <wwijsman@live.nl>
2022-02-01 20:49:53 +0100

    Clean up constants.hpp

--- src/constants.hpp
+++ src/constants.hpp
@@ -3,6 +3,14 @@
 
 inline constexpr float ANALOG_DEADZONE_MULTIPLIER = 0.5;
 
+inline constexpr int DROP_TIME = 300;
+
+inline constexpr int MATCH_TIME = 500;
+inline constexpr int MATCH_STEPS = 30;
+inline constexpr float MATCH_DELAY = MATCH_TIME/MATCH_STEPS;
+
+// Everything below this are defaults
+
 inline constexpr int BOARD_WIDTH = 8;
 inline constexpr int BOARD_HEIGHT = 7;
 
@@ -15,18 +23,9 @@ inline constexpr int SCREEN_WIDTH = 480;
 inline constexpr int SCREEN_HEIGHT = 272;
 inline constexpr int SHELL_SIZE = 32;
 #else
-inline constexpr int SCREEN_WIDTH = 2560;
-inline constexpr int SCREEN_HEIGHT = 1440;
+inline constexpr int SCREEN_WIDTH = 1920;
+inline constexpr int SCREEN_HEIGHT = 1080;
 inline constexpr int SHELL_SIZE = 128;
 #endif
-
-inline constexpr int FONT_SIZE = SHELL_SIZE/2;
-inline constexpr int FONT_SIZE_TITLE = SHELL_SIZE;
-
-inline constexpr int DROP_TIME = 300;
-
-inline constexpr int MATCH_TIME = 500;
-inline constexpr int MATCH_STEPS = 30;
-inline constexpr float MATCH_DELAY = MATCH_TIME/MATCH_STEPS;
 
 #endif // CONSTANTS_HPP
