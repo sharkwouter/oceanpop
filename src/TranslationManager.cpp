e16c2bc90a7c55fc1064bc82d7d0371cdd5e53a7
Wouter Wijsman <wwijsman@live.nl>
2024-07-18 10:47:44 +0200

    Add basic code to TranslationManager

--- src/TranslationManager.cpp
+++ src/TranslationManager.cpp
@@ -1,9 +1,42 @@
 #include "TranslationManager.hpp"
 
-TranslationManager::TranslationManager() {
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdlib.h>
+#include <iostream>
 
+#include <tinygettext/po_parser.hpp>
+#include <tinygettext/tinygettext.hpp>
+#include <tinygettext/unix_file_system.hpp>
+
+#include "utils.hpp"
+
+TranslationManager::TranslationManager() : dictionary_manager(tinygettext::DictionaryManager(std::unique_ptr<tinygettext::FileSystem>(new tinygettext::UnixFileSystem))) {
+    loadTranslations();
 }
 
 TranslationManager::~TranslationManager() {
     
+}
+
+void TranslationManager::loadTranslations() {
+    std::string system_language = getSystemLanguage();
+    std::string language_dir = getResourcePath("assets/languages/");
+    
+    dictionary_manager.add_directory(language_dir);
+    tinygettext::Language language = tinygettext::Language::from_name(system_language);
+    if (language) {
+        dictionary_manager.set_language(language);
+    } else {
+        SDL_Log("Couldn't load language %s", system_language.c_str());
+    }
+}
+
+std::string TranslationManager::getSystemLanguage() {
+    return "";
+}
+
+std::string TranslationManager::translate(std::string input) {
+    return dictionary_manager.get_dictionary().translate(input);
 }
--- src/TranslationManager.hpp
+++ src/TranslationManager.hpp
@@ -1,14 +1,21 @@
 #ifndef TRANSLATIONMANAGER_H
 #define TRANSLATIONMANAGER_H
 
-#include "tinygettext/tinygettext.hpp"
+#include <tinygettext/tinygettext.hpp>
 
 class TranslationManager {
 private:
+    tinygettext::DictionaryManager dictionary_manager;
+    bool language_set = false;
+
+    std::string getSystemLanguage();
+    void loadTranslations();
 
 public:
     TranslationManager();
     ~TranslationManager();
+
+    std::string translate(std::string input); 
 };
 
 #endif // TRANSLATIONMANAGER_H
