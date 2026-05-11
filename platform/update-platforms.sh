307dd84cbeb1b019aca0f05c551d9468a736511c
Wouter Wijsman <wwijsman@live.nl>
2026-05-11 16:21:15 +0200

    Do not create ogg files for PSP when running update-platforms

--- platform/update-platforms.sh
+++ platform/update-platforms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./add-new-platform.sh psp 480x272 ogg
+./add-new-platform.sh psp 480x272
 ./add-new-platform.sh vita 960x544
 ./add-new-platform.sh wii 640x480
 ./add-new-platform.sh ps2 640x448 wav
